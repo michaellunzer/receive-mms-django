ede1a7063d740b49e4dbc2a28556983ea8b1c5eb
Michael Lunzer <michaellunzer@gmail.com>
2020-10-06 22:17:16 -0700

    updated whitenoise

--- receive_mms/settings.py
+++ receive_mms/settings.py
@@ -124,4 +124,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "core/static/")
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
